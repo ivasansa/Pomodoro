a0f190945d78704e37dea7f9eceb5d0eb5057cdf
ivsasa1516daw2 <ivsasa1516daw2@gmail.com>
2016-03-08 13:53:47 +0100

    falta ordenar y webStorage

--- app/Tasca.model.js
+++ app/Tasca.model.js
@@ -1,11 +1,12 @@
 (function(app) {
   app.Tasca = Tasca;
 
-  function Tasca(nom, acabat, fechaCreacion, temps) {
+  function Tasca(nom, acabat, fechaCreacion, temps, paused) {
     this.nom = nom;
     this.acabat = acabat;
     this.fechaCreacion = fechaCreacion;
     this.temps = temps;
+    this.paused = paused;
   }
 
 
